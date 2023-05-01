8c59269e045190d0331fda948cae8c8924304eac
Sebastian Rodriguez <sebastianrodriguez@Sebastians-MacBook-Pro-3.local>
2023-05-01 12:49:10 -0600

    Working changing line

--- wakedemo/wakedemo.c
+++ wakedemo/wakedemo.c
@@ -16,6 +16,8 @@
 
 char blue = 31, green = 0, red = 31;
 unsigned char step = 0;
+double a = 1;
+char swing = 1;
 
 static char 
 switch_update_interrupt_sense()
@@ -46,33 +48,7 @@ switch_interrupt_handler()
   switches = ~p2val & SWITCHES;
 }
 
-
 // axis zero for col, axis 1 for row
-
-short drawPos[2] = {1,10}, controlPos[2] = {2, 10};
-short colVelocity = 1, colLimits[2] = {1, screenWidth/2};
-
-void
-draw_ball(int col, int row, unsigned short color)
-{
-  fillRectangle(col-1, row-1, 3, 3, color);
-}
-
-
-void
-screen_update_ball()
-{
-  for (char axis = 0; axis < 2; axis ++) 
-    if (drawPos[axis] != controlPos[axis]) /* position changed? */
-      goto redraw;
-  return;			/* nothing to do */
- redraw:
-  draw_ball(drawPos[0], drawPos[1], COLOR_BLUE); /* erase */
-  for (char axis = 0; axis < 2; axis ++) 
-    drawPos[axis] = controlPos[axis];
-  draw_ball(drawPos[0], drawPos[1], COLOR_WHITE); /* draw */
-}
-  
 
 short redrawScreen = 1;
 u_int controlFontColor = COLOR_GREEN;
@@ -83,16 +59,6 @@ void wdt_c_handler()
 
   secCount ++;
   if (secCount >= 25) {		/* 10/sec */
-   
-    {				/* move ball */
-      short oldCol = controlPos[0];
-      short newCol = oldCol + colVelocity;
-      if (newCol <= colLimits[0] || newCol >= colLimits[1])
-	colVelocity = -colVelocity;
-      else
-	controlPos[0] = newCol;
-    }
-
     {				/* update hourglass */
       if (switches & SW3) green = (green + 1) % 64;
       if (switches & SW2) blue = (blue + 2) % 32;
@@ -118,11 +84,12 @@ void main()
   configureClocks();
   lcd_init();
   switch_init();
+  //buzzer_init();
   
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);	              /**< GIE (enable interrupts) */
   
-  clearScreen(COLOR_BLUE);
+  clearScreen(COLOR_WHITE);
   while (1) {			/* forever */
     if (redrawScreen) {
       redrawScreen = 0;
@@ -135,36 +102,45 @@ void main()
 }
 
 void
-screen_update_hourglass()
+screen_update_line()
 {
   static unsigned char row = screenHeight / 2, col = screenWidth / 2;
-  static char lastStep = 0;
-  
-  if (step == 0 || (lastStep > step)) {
-    clearScreen(COLOR_BLUE);
-    lastStep = 0;
-  } else {
-    for (; lastStep <= step; lastStep++) {
-      int startCol = col - lastStep;
-      int endCol = col + lastStep;
-      int width = 1 + endCol - startCol;
-      
-      // a color in this BGR encoding is BBBB BGGG GGGR RRRR
-      unsigned int color = (blue << 11) | (green << 5) | red;
-      
-      fillRectangle(startCol, row+lastStep, width, 1, color);
-      fillRectangle(startCol, row-lastStep, width, 1, color);
+  static char lineLength = 10;
+  for (int x = 0; x <= lineLength; x++) {
+    unsigned int color = (blue << 11) | (green << 5) | red;
+    if(a != 0){
+      double y = 0;
+      if(a > 0){
+	y = x/a;
+	drawPixel(col+x, row-y, color);
+      }
+      else{
+        y = -x/a;
+        drawPixel(col-x, row-y, color);
+      }
+    }
+    else{
+      drawPixel(col+x, row, color);
     }
   }
-}  
+  clearScreen(COLOR_WHITE);
+}
 
 
     
 void
 update_shape()
 {
-  screen_update_ball();
-  screen_update_hourglass();
+  screen_update_line();
+  if(a >= 1 || a <= -1){
+    swing = !swing;
+  }
+  if(swing){
+    a += .1;
+  }
+  else{
+    a -= .1;
+  }
 }
    
 
